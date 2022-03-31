d5fa0f57de0326b07ad84ac74b6dcd90f56c0318
Chetan Dhiman <chetan.arethos@gmail.com>
2022-03-31 14:46:10 +0530

    added navbar

---
+++ component/Carousel/carousel.js
@@ -0,0 +1,25 @@
+function Carousel() {
+  return (
+    <div>
+      <div
+        id="carouselExampleSlidesOnly"
+        class="carousel slide"
+        data-ride="carousel"
+      >
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <p>First Courseel</p>
+          </div>
+          <div class="carousel-item">
+            <p>Second Coursel</p>
+          </div>
+          <div class="carousel-item">
+            <p>Third Coursel</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Carousel;
---
+++ component/navbar/Navbar.js
@@ -0,0 +1,64 @@
+// import react from "react";
+
+function Navbar() {
+  return (
+    <div>
+      {/* <button className="btn btn-danger"></button> */}
+      <nav className="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-center">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Navbar
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo02"
+            aria-controls="navbarTogglerDemo02"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse d-flex justify-content-end"
+            id="navbarTogglerDemo02"
+          >
+            <ul className="navbar-nav  mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Features
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Portfolio
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Blog
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Contact Us
+                </a>
+              </li>
+              <button type="button" class="btn btn-info">
+                Hire Us
+              </button>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
